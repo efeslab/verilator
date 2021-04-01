ee230a895c5b3f57c6b3fe953893bb7b93964a67
Jiacheng Ma <i@jcma.me>
2021-04-01 15:20:36 -0400

    disable splitting always blocks

--- src/Verilator.cpp
+++ src/Verilator.cpp
@@ -529,8 +529,8 @@ static void process() {
 			V3Dead::deadifyDTypesScoped(v3Global.rootp());
 			v3Global.checkTree();
 			V3Active::activeAll(v3Global.rootp());
-			V3Split::splitAlwaysAll(v3Global.rootp());
-			V3SplitAs::splitAsAll(v3Global.rootp());
+			//V3Split::splitAlwaysAll(v3Global.rootp());
+			//V3SplitAs::splitAsAll(v3Global.rootp());
 			V3Const::constifyAll(v3Global.rootp());
 			//V3Unknown::unknownAll(v3Global.rootp());
 			V3Dead::deadifyDTypesScoped(v3Global.rootp());
